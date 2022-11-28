f645b14d25e1f74d55ec0d42cef523109c5ab2d4
DingKKK <623812325@qq.com>
2022-11-29 00:19:53 +0800

    test revert version3

--- confirmation/confirm.cpp
+++ confirmation/confirm.cpp
@@ -36,14 +36,8 @@ confirm::~confirm()
 
 void confirm::on_Confirm_clicked()
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    qDebug()<<"test revert version1";
     qDebug()<<"test revert version2";
-=======
->>>>>>> parent of 7dad79b... test revert version1
-=======
->>>>>>> parent of 7dad79b... test revert version1
+    qDebug()<<"test revert version3";
     f1compare.clear();
     f2compare.clear();
     ui->fN1->clear();
